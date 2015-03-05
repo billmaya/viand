56a2c25362c32c683ddb08ed5708a888700e5d54
Bill Maya <bill.maya@gmail.com>
2015-03-05 06:14:38 -0500

    Added Item class to BuyPage. Created sample list and ListView on BuyPage. Set ListView as StackLayout child.

--- Viand/BuyPage.cs
+++ Viand/BuyPage.cs
@@ -1,21 +1,47 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Viand
 {
 	public class BuyPage : ContentPage
 	{
+		class Item
+		{
+			public string Name { get; set;}
+			public bool Buy { get; set; }
+
+			public Item(string name, bool buy) {
+				this.Name = name;
+				this.Buy = buy;
+			}
+		}
+
 		public BuyPage()
 		{
 			Title = "Buy";
 			Icon = "19-checkmark.png";
 
+			List<Item> items = new List<Item> {
+				new Item("Milk", true),
+				new Item("Eggs", true),
+				new Item("Carrots", true),
+				new Item("Raisins", false),
+				new Item("Bread", false)
+			};
+
+			ListView itemView = new ListView {
+				RowHeight = 60,
+				ItemsSource = items,
+				ItemTemplate = new DataTemplate(typeof(TextCell))
+			};
+
+			itemView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
 			Content = new StackLayout { 
+				VerticalOptions = LayoutOptions.FillAndExpand,
 				Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0),
-				Children = {
-					new Label { Text = "Hello BuyPage" }
-				}
+				Children = { itemView }
 			};
 		}
 	}
