e2ead28b92231d0d4fdce9e142f5835bf1b2ec23
Bill Maya <bill.maya@gmail.com>
2015-04-04 09:23:59 -0400

    Added Title property to Item class.

--- Viand/Item.cs
+++ Viand/Item.cs
@@ -7,11 +7,13 @@ namespace Viand
 		public string Name { get; set; }
 		public bool Buy { get; set; }
 		public int Quantity { get; set; }
+		public string Title { get; private set; }
 
 		public Item(string name, bool buy, int quantity = 1 ) {
 			this.Name = name;
 			this.Buy = buy;
 			this.Quantity = quantity;
+			this.Title = name.Substring(0, 1);
 		}
 	}
 }
