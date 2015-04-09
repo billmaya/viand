b0556007a173aa8b598516612d10054be0d538c0
Bill Maya <bill.maya@gmail.com>
2015-04-09 06:24:06 -0400

    Made Name, Buy, and Quantity properties setters private.

--- Viand/Item.cs
+++ Viand/Item.cs
@@ -4,9 +4,9 @@ namespace Viand
 {
 	public class Item
 	{
-		public string Name { get; set; }
-		public bool Buy { get; set; }
-		public int Quantity { get; set; }
+		public string Name { get; private set; }
+		public bool Buy { get; private set; }
+		public int Quantity { get; private set; }
 		public string Label { get; private set; }
 
 		public Item(string name, bool buy, int quantity = 1 ) {
