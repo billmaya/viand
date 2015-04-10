f3078f4f8eea0ec6ec9d45db27dbb5395e4bddc6
Bill Maya <bill.maya@gmail.com>
2015-04-10 06:37:50 -0400

    Implemented IComparable<Item> interface on Item with corresponding CompareTo method.

--- Viand/Item.cs
+++ Viand/Item.cs
@@ -2,18 +2,24 @@
 
 namespace Viand
 {
-	public class Item
+	public class Item : IComparable<Item>
 	{
 		public string Name { get; private set; }
 		public bool Buy { get; private set; }
 		public int Quantity { get; private set; }
 		public string Label { get; private set; }
 
-		public Item(string name, bool buy, int quantity = 1 ) {
+		public Item(string name, bool buy, int quantity = 1 ) 
+		{
 			this.Name = name;
 			this.Buy = buy;
 			this.Quantity = quantity;
 			this.Label = Name[0].ToString; 
+		}
+			
+		public int CompareTo(Item other) 
+		{
+			return Name.CompareTo(other.Name);
 		}
 	}
 }
