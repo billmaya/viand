8672e1b58d22d56c6fb367660d795322cca34f6e
Bill Maya <bill.maya@gmail.com>
2015-04-11 07:14:07 -0400

    Added ItemCollection to Viand project in order to sort Add page items.

---
+++ Viand/ItemCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Viand
+{
+	public class ItemCollection
+	{
+		public string Title { get; set; }
+
+		public ItemCollection(string title)
+		{
+			Title = title;
+		}
+
+		public static IEnumerable<Item> GetSortedData()
+		{
+			List<Item> allItems;
+			IEnumerable<Item> addItems;
+
+			if (Application.Current.Properties.ContainsKey("Items")) {
+				allItems = (List<Item>)Application.Current.Properties["Items"];
+				addItems = allItems.Where(item => item.Buy != true);
+			}
+
+			addItems.OrderBy(x => x.Name);
+
+			return addItems;
+		}
+	}
+}
+
