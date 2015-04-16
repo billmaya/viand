1bd1b22241bc937e31f69ebbd3af912f5b791a85
Bill Maya <bill.maya@gmail.com>
2015-04-16 07:07:37 -0400

    Hooked up Label and Switch to OnDestinationSwitchToggled method in code behind.

--- Viand/AddItemPage.xaml.cs
+++ Viand/AddItemPage.xaml.cs
@@ -11,6 +11,15 @@ namespace Viand
 		{
 			InitializeComponent();
 		}
+
+		void OnDestinationSwitchToggled(object sender, EventArgs args)
+		{
+			if (destinationSwitch.IsToggled == true) {
+				destinationLabel.Text = "Save to Add list";
+			} else {
+				destinationLabel.Text = "Save to Buy list";
+			}
+		}
 	}
 }
 
