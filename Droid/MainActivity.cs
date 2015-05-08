30530c5ff81fa2b5ae22ea1e4ee26328a39db2d2
Bill Maya <bill.maya@gmail.com>
2015-05-07 21:24:38 -0400

    Changed MainActivity Label from "Viand.Droid" to "Viand".

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace Viand.Droid
 {
-	[Activity(Label = "Viand.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "Viand", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate(Bundle bundle)
